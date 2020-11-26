bfda877ef34878d4e25e73bb283e88f2442b4cb8
Jan Puig <janpuigcapella@gmail.com>
2020-11-26 16:31:28 +0100

    show-hide images

---
+++ show-hide-image/show_hide.js
@@ -0,0 +1,43 @@
+var backgrounds= document.getElementsByClassName("backImage");
+var imgs= document.getElementById('game');
+
+window.onload=function(){
+    console.log(backgrounds[0])
+    setTimeout(hideAllImages, 3000);
+}
+
+imgs.addEventListener('click', imageClick)
+
+function hideAllImages(){
+    for(let i=0; i<backgrounds.length; i++){
+         hideImage(backgrounds[i]);
+    }
+}
+
+function hideImage(back){
+    back.classList.add("showBackground");
+}
+
+function showImage(back){
+    back.classList.remove("showBackground");
+}
+
+function imageClick(event){
+    if(!event.target.classList.contains('buttonImage')){
+        toggleImage(event.target);
+    }
+    else{
+        back=event.target.getElementsByClassName('backImage')[0];
+        toggleImage(back);
+    }
+
+    function toggleImage(b){
+        console.log(b);
+        if(b.classList.contains('showBackground')){
+            showImage(b);
+        }else{
+            hideImage(b);
+        }
+    }
+
+}
