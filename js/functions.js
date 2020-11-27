8d71a5c97d5d998ee8706761439d9e7e0b465e49
Jan Puig <janpuigcapella@gmail.com>
2020-11-27 12:38:06 +0100

    order data and js

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+const grid = document.querySelector(".grid");
+const randomImgArray = randomImageOrder(imgArray);
+function createGrid() {
+  for (let i = 0; i < imgArray.length * 2; i++) {
+    const card = document.createElement("button");
+    card.style.backgroundImage = imgArray[i].img;
+    card.style.backgroundSize = "cover";
+    card.setAttribute("class", "buttonImage");
+    const btnDiv = document.createElement("div");
+    btnDiv.setAttribute("class", "backImage");
+    card.appendChild(btnDiv);
+    card.setAttribute("id", "button" + i);
+    grid.appendChild(card);
+  }
+}
+
+function randomImageOrder(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * i);
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+createGrid();
