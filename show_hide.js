178aa34ba0a1d3623379374b1912a99f455cf6b2
Jan Puig <janpuigcapella@gmail.com>
2020-11-27 09:33:37 +0100

    check if same images and hide if they aren't

--- show_hide.js
+++ show_hide.js
@@ -1,5 +1,6 @@
 var backgrounds= document.getElementsByClassName("backImage");
 var imgs= document.getElementById('gameGrid');
+var pairOfImages=[];
 
 window.onload=function(){
     console.log(backgrounds[0])
@@ -17,6 +18,12 @@ function hideAllImages(){
 function hideImage(back){
     back.classList.add("showBackground");
 }
+function hidePairOfImg(){
+    pairOfImages.forEach(btn=>{
+        back=btn.getElementsByClassName('backImage')[0];
+        hideImage(back);
+    })
+}
 
 function showImage(back){
     back.classList.remove("showBackground");
@@ -32,12 +39,33 @@ function imageClick(event){
     }
 
     function toggleImage(b){
-        console.log(b);
         if(b.classList.contains('showBackground')){
             showImage(b);
+            let full=addImageToArray(b.parentNode);
+            if(full){
+                if(areTheSameImage()){
+                    
+                }else{
+                    setTimeout(hidePairOfImg, 3000);
+                }
+            }
         }else{
-            hideImage(b);
+            //hideImage(b);
         }
+        console.log(pairOfImages);
     }
+}
 
+function areTheSameImage(){
+    if(pairOfImages[0].style.backgroundImage==pairOfImages[1].style.backgroundImage){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//returns false if has 1 object after adding and true if has 2;
+function addImageToArray(bttn){
+    pairOfImages.push(bttn);
+    return pairOfImages.length==2;
 }
