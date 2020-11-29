b275adbceafc88450cf7f8461cc965b4644a0ff7
Jan Puig <janpuigcapella@gmail.com>
2020-11-30 00:11:15 +0100

    bug (same position of images each time)

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,13 @@
 
 const grid = document.querySelector(".grid");
-const randomImgArray = randomImageOrder(imgArray);
+let randomImgArray = randomImageOrder(imgArray);
 
   document.getElementById("start").style.display = "block";
   document.getElementById('final').style.display='none';
   document.getElementById('gameSection').style.display='none';
 
 function createGrid() {
+  randomImgArray = randomImageOrder(imgArray);
   for (let i = 0; i < randomImgArray.length; i++) {
     const card = document.createElement("button");
     card.style.backgroundImage = randomImgArray[i].img;
